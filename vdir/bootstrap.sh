1623b71a98506983c48f231a8e9ba6455724d995
NikolaMandic <mn080202@mail.ru>
2013-09-09 06:33:55 +0100

    minor changes

--- vdir/bootstrap.sh
+++ vdir/bootstrap.sh
@@ -10,8 +10,8 @@ git clone https://github.com/NikolaMandic/ld.git
 cd ./ld 
 apt-get install nodejs -y
 apt-get install npm -y
-npm install express
-npm install socket.io
-npm install bower 
+npm install -g express
+npm install -g socket.io
+npm install -g bower 
 bower install
 nodejs ws.js
