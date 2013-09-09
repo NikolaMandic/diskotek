163977e95bf86a76096e2c9c1a3201a5598d20f7
NikolaMandic <mn080202@mail.ru>
2013-09-09 06:18:10 +0100

    added stuff

---
+++ vdir/bootstrap.sh
@@ -0,0 +1,17 @@
+apt-get install python-software-properties -y
+add-apt-repository ppa:chris-lea/node.js -y 
+apt-get update
+apt-get install gcc-arm-linux-gnueabi -y
+apt-get install qemu-user-static -y
+apt-get install gdb-multiarch -y
+export QEMU_LD_PREFIX=/usr/arm-linux/gnueabi 
+apt-get install git -y
+git clone https://github.com/NikolaMandic/ld.git
+cd ./ld 
+apt-get install nodejs -y
+apt-get install npm -y
+npm install express
+npm install socket.io
+npm install bower 
+bower install
+nodejs ws.js
