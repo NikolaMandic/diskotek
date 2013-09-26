0b9b4e58b0e33f6a10dc4ebbaa60962adf2da438
NikolaMandic <mn080202@gmail.com>
2013-09-26 18:51:27 +0100

    backup commit TogetherJS modified

--- app/scripts/controllers/debugController.js
+++ app/scripts/controllers/debugController.js
@@ -1,5 +1,6 @@
 angular.module('ldApp')
   .controller('debugController', function ($rootScope,$scope,$http,Data) {
+//    $(document).trigger("routeChanged");
     $scope.data=Data;
     $scope.toggleBreakpoint = function(address,thing) {
       thing.hasBreakpoint = thing.hasBreakpoint?false:true;
